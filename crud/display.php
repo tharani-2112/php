7a951c92989d594c86c6c7d6d0f8608811f5420c
Tharani Harichandran <tharani.harichandran@aspiresys.com>
2021-08-06 19:57:12 +0530

    crud application

---
+++ crud/display.php
@@ -0,0 +1,62 @@
+<?php include'connect.php' ;?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>    
+    <title>Registration Form</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  </head>
+  <body>
+    <div class="container">
+      <button><a href ="index.php">Add user</a></button>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Sl.no</th>
+              <th>Name</th>
+              <th >Email</th>
+              <th>Mobile</th>        
+              <th>Gender</th>
+              <th>Country</th>
+              <th>File</th>
+              <th>Process</th>
+            </tr>
+          </thead>
+          <tbody>
+          <?php
+          $sql="SELECT * FROM register";
+          $result=mysqli_query($mysqli,$sql);
+        
+          if($result)
+          {
+            $row = mysqli_fetch_assoc($result);
+            $i=1;
+            while($row=mysqli_fetch_assoc($result))
+            { 
+                $id=$row['id'];
+                $name=$row['name'];
+                $email=$row['email'];
+                $mobile=$row['mobile'];   
+                $gender=$row['gender']; 
+                $country=$row['country'];
+                $file=$row['file'];           
+                echo '
+                <tr>
+                  <th scope="row">'.$i.'</th>
+                    <td>'.$name.'</td>
+                    <td>'.$email.'</td>
+                    <td>'.$mobile.'</td>                   
+                    <td>'.$gender.'</td>
+                    <td>'.$country.'</td>
+                    <td>'.$file.'</td>
+                    <td><button><a href="update.php?updateid='.$id.'">Update</a></button>
+                    <button><a href="delete.php?deleteid='.$id.'">Delete</a></button></td>
+                </tr>';
+                $i++;
+                }        
+            }
+          ?>        
+          </tbody>
+      </table>
+    </div>    
+  </body>
+</html>
---
+++ crud/login.php
@@ -0,0 +1,45 @@
+<?php
+include 'connect.php';
+session_start();
+
+if(isset($_POST['submit']))
+{
+    extract($_POST);
+    $sql=mysqli_query($mysqli,"SELECT * FROM register WHERE Email='$email' and Password=md5('$password')");
+    $row  = mysqli_fetch_array($sql);
+    if(is_array($row))
+    {
+        $_SESSION["id"] = $row['id'];
+		$_SESSION["name"]=$row['name'];
+        $_SESSION["email"]=$row['email']; 
+        $_SESSION["mobile"]=$row['mobile']; 
+		$_SESSION["password"]=$row['$password'];
+        $_SESSION["gender"]=$row['gender'];
+        $_SESSION["country"]=$row['country'];
+        $_SESSION["file"]=$row['file'];
+        header("Location: profile.php"); 
+    }
+    else
+    {
+        echo "Invalid Email ID/Password";
+    }
+}
+?>
+<!DOCTYPE html>
+<head>
+    <title>Login Page</title>
+    <link rel="stylesheet" type="text/css" href="http://localhost/crud/css/register.css">
+</head>
+<body>
+    <div id="container">
+    <img id="image" src="http://localhost/crud/css/usericon.jpg" height="70" width='70'/>
+    <form class="form" method="post">
+      <h5 class="heading">LOGIN HERE</h5>
+        <input class="inputs"  name="email" id="Email" type="email" placeholder="Email address"/><br><br>
+        <input class="inputs" name="password" id="ps2" type="password" placeholder="Password"/><br><br>
+        <button id="button" type="submit" name="submit">Login</button><br><br>
+        <a href="index.php" style="color:white;">Don't have an account</a>
+    </form>
+    </div>
+</body>
+</html>
